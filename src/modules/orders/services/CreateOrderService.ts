ca56baa01cff7c99a837f4a0440b3ff6f4a89c04
Carlos Krueger <crlalexandrek@gmail.com>
2020-10-13 23:10:43 -0300

    feat: challenge completed

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,57 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+
+    if (!customer) {
+      throw new AppError("You can't create an order with an invalid customer.");
+    }
+
+    const selectedProducts = await this.productsRepository.findAllById(
+      products,
+    );
+
+    if (products.length > selectedProducts.length) {
+      throw new AppError("You can't create an order with invalid products");
+    }
+
+    const storedProducts = await this.productsRepository.findAllById(products);
+
+    const pricedProducts = products.map(product => {
+      const index = storedProducts.findIndex(it => it.id === product.id);
+
+      if (product.quantity > storedProducts[index].quantity) {
+        throw new AppError(
+          "You can't place an order with a product that exceeds the amount stored",
+        );
+      }
+
+      return {
+        product_id: product.id,
+        quantity: product.quantity,
+        price: storedProducts[index].price,
+      };
+    });
+
+    const order = await this.ordersRepository.create({
+      customer,
+      products: pricedProducts,
+    });
+
+    await this.productsRepository.updateQuantity(products);
+
+    return order;
   }
 }
 
